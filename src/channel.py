95186842ae3f89b8613f2fab1d848c752918c93b
Samantha Tse <z5363701@ad.unsw.edu.au>
2021-09-30 22:05:18 +1000

    fix: removed the sample return provided for channel_messages_v1

--- src/channel.py
+++ src/channel.py
@@ -119,19 +119,6 @@ def channel_messages_v1(auth_user_id, channel_id, start):
         'end': end
     }
 
-    return {
-        'messages': [
-            {
-                'message_id': 1,
-                'u_id': 1,
-                'message': 'Hello world',
-                'time_created': 1582426789,
-            }
-        ],
-        'start': 0,
-        'end': 50,
-    }
-
 def channel_join_v1(auth_user_id, channel_id):
     ## checks auth_user_id exists
     check_user_exists(auth_user_id)
