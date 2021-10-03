0b3bcccd3b9e274f7fc8dab1c5813685acb0dba7
Dhruv Agrawal <dhruv.gravity@gmail.com>
2021-10-03 15:13:42 +1100

    style: removed trailing whitespace, added module docstring, fixed indentation for dicts and docs

--- src/channels.py
+++ src/channels.py
@@ -1,22 +1,32 @@
-from src.data_operations import get_channel_ids, get_channel, get_user_ids, add_channel
+'''
+Controls creation of channels, retrieves user and global channel lists
+
+Functions:
+    channels_list_v1(auth_user_id: int) -> dict
+    channels_listall_v1(auth_user_id: int) -> dict
+    channels_create_v1(auth_user_id: int, name: str, is_public: bool) -> dict
+'''
+
+from src.data_operations import get_channel_ids, get_channel, add_channel
 from src.other import check_user_exists
-from src.error import InputError, AccessError
+from src.error import InputError
 
 def channels_list_v1(auth_user_id):
     '''
-    returns a list of all the channels with their channel id's and channel name that the user
+    Returns a list of all the channels with their channel id's and channel name that the user
     is a member of
 
     Arguments:
-        auth_user_id (int)      - id of user
-        
+        auth_user_id (int): id of user
+
     Exceptions:
-        InputError              - None
-        AccessError             - invalid auth_id
+        AccessError: Occurs when:
+                        - invalid auth_user_id
 
     Return Value:
-        channel_list : list
+        { channels (list): list of all channel dicts a given user is member of }
     '''
+
     ## checks auth_user_id exists
     check_user_exists(auth_user_id)
 
@@ -26,9 +36,9 @@ def channels_list_v1(auth_user_id):
         if auth_user_id in get_channel(channel)['members']:
             channel_list.append({
                 'channel_id': channel,
-                'name': get_channel(channel)['name']
+                'name'      : get_channel(channel)['name']
             })
-            
+
     return {
         'channels': channel_list,
     }
@@ -45,16 +55,9 @@ def channels_listall_v1(auth_user_id):
         AccessError             - invalid auth_id
 
     Return Value:
-        { channel_id        : int,
-        name : 
-            { name          : str,
-            owner         : str, 
-            is_public     : bool,
-            members       : list,
-            message_ids   : list
-            }        
-        }
+        { channels (list): list of all channel dicts a given user is member of }
     '''
+
     ## checks auth_user_id exists
     check_user_exists(auth_user_id)
 
@@ -63,7 +66,7 @@ def channels_listall_v1(auth_user_id):
     for channel in get_channel_ids():
         all_channels.append({
             'channel_id': channel,
-            'name': get_channel(channel)['name']
+            'name'      : get_channel(channel)['name']
         })
 
     return {
@@ -72,30 +75,32 @@ def channels_listall_v1(auth_user_id):
 
 def channels_create_v1(auth_user_id, name, is_public):
     '''
-    creates a new channel with a given name that is public or private. The user that
-    creates it automatically joins the channel
+    Creates a new channel with a given name that is public or private. The user that
+    creates it automatically joins the channel as an owner member
 
     Arguments:
-        auth_user_id (int)      - id of user that is creating the channel
-        name (str)              - name of the new channel
-        is_public (bool)        - the privacy status of the channel
+        auth_user_id (int): id of user that is creating the channel
+        name         (str): name of the new channel
+        is_public   (bool): the privacy status of the channel
 
     Exceptions:
-        InputError              - Occurs when:
-                                    - user id does not exist
-                                    - invalid channel name size
-        AccessError             - invalid auth_id
+        InputError: Occurs when:
+                        - user id does not exist
+                        - invalid channel name size
+        AccessError: invalid auth_id
 
     Return Value:
-        new_channel_id : int
+        { channel_id (int): unique id that references the created channel }
     '''
+
     ## checks auth_user_id exists
     check_user_exists(auth_user_id)
-    
+
     ## check channel name between 1 and 20 characters
     if not 1 <= len(name) <= 20:
         raise InputError('Invalid channel name size')
 
+    ## get a new id for the channel and add channel to system
     new_channel_id = len(get_channel_ids()) + 1
     add_channel(new_channel_id, name, auth_user_id, is_public)
 
