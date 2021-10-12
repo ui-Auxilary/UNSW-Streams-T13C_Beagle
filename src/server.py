a81525736c3fa3104e9e5417ef2e3d541a94224e
Dhruv Agrawal <dhruv.gravity@gmail.com>
2021-10-13 04:54:23 +1100

    feat: added routes for register, login, sethandle, user_profile, users_all functions

--- src/server.py
+++ src/server.py
@@ -6,6 +6,11 @@ from flask_cors import CORS
 from src.error import InputError
 from src import config
 
+from src.users import users_all
+from src.user import user_profile, user_profile_sethandle
+from src.auth import auth_register_v1, auth_login_v1
+from src.other import clear_v1
+
 def quit_gracefully(*args):
     '''For coverage'''
     exit(0)
@@ -38,6 +43,60 @@ def echo():
     return dumps({
         'data': data
     })
+
+@APP.route("/auth/login/v2", methods=['POST'])
+def login_user_session():
+    ## get user's token and the handle they want to update to
+    user_email = request.args.get('email')
+    user_password = request.args.get('password')
+
+    return dumps(auth_login_v1(user_email, user_password))
+
+
+@APP.route("/auth/register/v2", methods=['POST'])
+def register_new_user():
+    ## get user's token and the handle they want to update to
+
+    user_email = request.args.get('email')
+    user_password = request.args.get('password')
+    name_first = request.args.get('name_first')
+    name_last = request.args.get('name_last')
+
+    return dumps(auth_register_v1(user_email, user_password, name_first, name_last))
+
+@APP.route("/users/all/v1", methods=['GET'])
+def get_all_users():
+    ## get user's token and the handle they want to update to
+    user_token = request.args.get('token')
+
+    return dumps(users_all(user_token))
+
+@APP.route("/user/profile/v1", methods=['GET'])
+def get_user_profile():
+    ## get user's token and the handle they want to update to
+    user_token = request.args.get('token')
+    user_id = int(request.args.get('u_id'))
+
+    return dumps(user_profile(user_token, user_id))
+
+
+@APP.route("/user/profile/sethandle/v1", methods=['PUT'])
+def update_user_handle():
+    ## get user's token and the handle they want to update to
+    user_token = request.args.get('token')
+    new_handle = request.args.get('handle_str')
+    
+    ## set the new handle
+    user_profile_sethandle(user_token, new_handle)
+
+    return dumps({
+    })
+
+@APP.route("/clear/v1", methods=['DELETE'])
+def clear_data_store():
+    clear_v1()
+    return dumps({})
+
 
 #### NO NEED TO MODIFY BELOW THIS POINT
 
