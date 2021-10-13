209638873396adf47f847a64f9460b7f63216127
Christian Lam <z5359842@ad.unsw.edu.au>
2021-10-13 18:33:46 +1100

    feat: added implementation for pyjwt token encoding and decoding

--- src/other.py
+++ src/other.py
@@ -7,11 +7,14 @@ Functions:
     check_user_exists(auth_user_id: str)
 '''
 
+import jwt    
+
 from src.data_operations import ( 
     get_user_ids,
     reset_data_store_to_default,
     get_user_from_token,
-    get_all_valid_tokens
+    get_all_valid_tokens,
+    add_session_token
 )
 from src.error import AccessError
 
@@ -44,11 +47,23 @@ def check_user_exists(auth_user_id):
         raise AccessError('Auth_user_id does not exist')
 
 def encode_token(user_id):
-    return str(user_id)
+    SECRET = "DHRUV_IS_SALTY"
+
+    encoded_token = jwt.encode({'user_id': user_id}, SECRET, algorithm='HS256')
+
+    ## Adds active token to database
+    add_session_token(encoded_token, user_id)
+
+    return encoded_token
+    
 
 def decode_token(token):
+    SECRET = "DHRUV_IS_SALTY"
+
     ## check if user is valid
     if token not in get_all_valid_tokens():
         raise AccessError(description='Invalid token')
     
-    return get_user_from_token(token)
+    user_id = jwt.decode(token, SECRET, algorithm='HS256')['user_id']
+
+    return user_id
