2870d70078146c05e343d3fe18df718bc7f42875
Christian Lam <z5359842@ad.unsw.edu.au>
2021-10-16 18:20:59 +1100

    feat: added routes for message_edit and message_remove functions

--- src/server.py
+++ src/server.py
@@ -10,7 +10,7 @@ from src.error import InputError
 from src.users import users_all
 from src.user import user_profile, user_profile_sethandle, user_profile_setname, user_profile_setemail
 from src.auth import auth_register_v1, auth_login_v1
-from src.message import message_send_v1
+from src.message import message_send_v1, message_edit_v1, message_remove_v1
 from src.other import clear_v1
 from src.channels import channels_create_v1, channels_list_v1, channels_listall_v1
 from src.channel import channel_details_v1, channel_join_v1, channel_invite_v1, channel_addowner_v1, channel_removeowner_v1, channel_messages_v1, channel_leave_v1
